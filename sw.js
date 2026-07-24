7752fa9f0cccfbd335b496f6cd334274ec8c9813
brunodiasef <brunopersonalsgp@gmail.com>
2026-07-24 10:29:25 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Service Worker - Desafio Geográfico
+const CACHE_NOME = 'desafio-geo-cache-v1';
+const ARQUIVOS_PARA_CACHE = [
+  './',
+  './index.html',
+  './dados-jogo.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (evento) => {
+  evento.waitUntil(
+    caches.open(CACHE_NOME).then((cache) => cache.addAll(ARQUIVOS_PARA_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evento) => {
+  evento.waitUntil(
+    caches.keys().then((nomes) =>
+      Promise.all(nomes.filter((nome) => nome !== CACHE_NOME).map((nome) => caches.delete(nome)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evento) => {
+  evento.respondWith(
+    caches.match(evento.request).then((respostaEmCache) => {
+      if (respostaEmCache) return respostaEmCache;
+      return fetch(evento.request)
+        .then((respostaDaRede) => {
+          const copia = respostaDaRede.clone();
+          caches.open(CACHE_NOME).then((cache) => cache.put(evento.request, copia));
+          return respostaDaRede;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
